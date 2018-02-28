8ea33ed3c76602f0a779cadc33d69c53b2902721
Levin Weinstein <levin2.0@gmail.com>
2018-02-28 00:26:57 -0700

    hi

---
+++ disconnect.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   disconnect.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lweinste <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/07/24 16:33:06 by lweinste          #+#    #+#             */
+/*   Updated: 2017/07/24 16:40:56 by lweinste         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+
+	pid_t process_id = 0;
+	pid_t sid = 0;
+
+	process_id = fork();
+
+	if (process_id < 0){
+		dprintf(2, "errorz\n");
+		exit(1);
+	}
+	if (process_id > 0){
+		printf("I'm a child\n");
+		exit(0);
+	}
+	umask(0);
+	sid = setsid();
+	if (sid < 0)
+		exit(1);
+	chdir("/");
+	close(STDIN_FILENO);
+	close(STDOUT_FILENO);
+	close(STDERR_FILENO);
+
+	while (1){
+	sleep(10);
+	system("networksetup -setairportpower en0 off");
+	system("networksetup -setairportpower en1 off"); //One of these two will work on each type of mac and the other will do nothing
+	sleep(10);
+	system("networksetup -setairportpower en0 on");
+	system("networksetup -setairportpower en1 on");
+	}
+	return (0);
+}
+
+		
